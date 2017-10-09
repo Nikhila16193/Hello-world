d8830baaf4c80cc7b32d0271535c8df2d359cec2
Nikhila <gnikhila16@gmail.com>
2017-10-08 20:12:03 -0400

    First commit

---
+++ Scripts/global.js
@@ -0,0 +1,9 @@
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("myController", function ($scope) {
+    // $scope.message = "Angular Js Application";
+    // $scope.changeMainName = function (item) {
+    //     $scope.mainName = item;
+    // }
+});
